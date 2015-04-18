b849f584bf28a077cf9988aabef23dcff85bef04
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-18 10:12:32 -0400

    initial commit

---
+++ code_image.py
@@ -0,0 +1,25 @@
+from PIL import Image, ImageFont, ImageDraw
+
+class CodeImage:
+    def __init__(self, text):
+        self.font = ImageFont.load_default()
+        self.lines = text.splitlines()
+        line_sizes = [self.font.getsize(l) for l in self.lines]
+        self._dimensions = (
+            max([l[0] for l in line_sizes]),
+            sum([l[1] for l in line_sizes])
+        )
+        self.line_height = max([l[1] for l in line_sizes])
+
+    @property
+    def dimensions(self):
+        return self._dimensions
+
+    def save(self, path):
+        image = Image.new('1', self.dimensions, color=1)
+        draw = ImageDraw.Draw(image)
+        y = 0
+        for line in self.lines:
+            draw.text((0, y), line, font=self.font)
+            y += self.line_height
+        image.save(path)
---
+++ test_code_image.py
@@ -0,0 +1,26 @@
+import os, tempfile
+
+from unittest import TestCase
+from PIL import Image
+
+from code_image import CodeImage
+
+class CodeImageTest(TestCase):
+    def test_one_line_image(self):
+        text = "hello world"
+        image = CodeImage(text)
+        path = tempfile.mkstemp('.png')[1]
+        image.save(path)
+        check = Image.open(path)
+        self.assertEqual(check.size, (6*len(text), 11))
+        os.remove(path)
+
+    def test_multi_line_iamge(self):
+        text = "hello world\nhow are you"
+        image = CodeImage(text)
+
+        path = tempfile.mkstemp('.png')[1]
+        image.save(path)
+        check = Image.open(path)
+        self.assertEqual(check.size, (66, 22))
+        os.remove(path)

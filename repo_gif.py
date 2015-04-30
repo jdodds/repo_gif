bfb5bb75b92fe16bd261a70c44878762057387c4
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-30 10:44:15 -0400

    add a way to check for binary data in the manner of the file command, and draw lines instead of text

--- repo_gif.py
+++ repo_gif.py
@@ -1,4 +1,4 @@
-import subprocess, tempfile, os, math
+import tempfile, os, math, resource
 
 import gifmaker
 
@@ -7,41 +7,47 @@ from PIL import Image, ImageFont, ImageDraw
 
 Dimensions = namedtuple('Dimensions', ['width', 'height'])
 
-class ImagesForFile:
-    def __init__(self, path, font=ImageFont.load_default()):
-        self._path = path
-        self._data = {}
-        self._font = font
-        self._dimensions = Dimensions(width=0, height=0)
-        self._line_height = 0
+def is_binary_string(s):
+    text_chars = (
+        bytearray([7, 8, 9, 10, 12, 13, 27]) +
+        bytearray(range(0x20, 0x7E)) +
+        bytearray(range(0x80, 0xFF))
+    )
+    return bool(s.translate(None, text_chars))
 
-    def add_commit_data(self, commit, data):
+class ImagesForFile:
+    def __init__(self, path):
+        self._path = path
+        self._commit_images = {}
+        self._seen_hashes = {}
+        self._dimensions = Dimensions(width=0, height=0)
+
+    def add_commit_data(self, commit, blob):
+        if blob.binsha in self._seen_hashes:
+            self._commit_images[commit] = self._commit_images[
+                self._seen_hashes[blob.binsha]
+            ]
+            return
+        data = blob.data_stream.read().splitlines()
         if data:
-            line_sizes = [self._font.getsize(l) for l in data]
-            tallest_line = max([l[1] for l in line_sizes])
-            widest_line = max([l[0] for l in line_sizes])
-            data_height = len(data)*tallest_line
-            if widest_line > 1000 or data_height > 1000:
-                return
+            widest_line = max(len(l) for l in data)
             self._dimensions = Dimensions(
                 width=max(widest_line, self._dimensions.width),
-                height=max(data_height, self._dimensions.height)
+                height=max(len(data), self._dimensions.height)
             )
-            self._line_height = max(tallest_line, self._line_height)
-        self._data[commit] = data
+        image = Image.new('1', self._dimensions, color=1)
+        draw = ImageDraw.Draw(image)
+        for y, line in enumerate(data):
+            draw.line([(0,y), (len(line), y)], fill=0)
+        self._commit_images[commit] = image
+        self._seen_hashes[blob.binsha] = commit
 
 
     def in_commit(self, commit):
-        return commit in self._data
+        return commit in self._commit_images
 
     def commit_image(self, commit):
-        lines = self._data[commit]
-        image = Image.new('1', self._dimensions, color=1)
-        draw = ImageDraw.Draw(image)
-        for i, line in enumerate(lines):
-            y_position = i*self._line_height
-            draw.text((0, y_position), line, font=self._font)
-        return image
+        return self._commit_images[commit]
 
     @property
     def width(self):
@@ -51,23 +57,26 @@ class ImagesForFile:
     def height(self):
         return self._dimensions.height
 
-
 def repo_gif(repo, outfile):
     commits = [repo.head.commit]
     while len(commits[-1].parents) != 0:
         commits.append(commits[-1].parents[0])
+    commits.reverse()
 
     file_images = {}
-    for commit in reversed(commits):
-        for f in commit.tree.traverse(lambda i,d: i.type=='blob'):
+    for commit in commits:
+        for f in commit.tree.traverse(
+                lambda i,d: i.type=='blob' and
+                not is_binary_string(i.data_stream.read(1024))
+        ):
             file_images.setdefault(f.path, ImagesForFile(f.path)).add_commit_data(
-                commit.hexsha,
-                f.data_stream.read().splitlines()
+                commit.hexsha, f
             )
 
-    widest = max(f.width for f in file_images.values())
-    tallest = max(f.height for f in file_images.values())
-    num_images = len(file_images)
+    images = file_images.values()
+    widest = max(f.width for f in images)
+    tallest = max(f.height for f in images)
+    num_images = len(images)
     images_per_row = math.ceil(math.sqrt(num_images))
     num_rows = math.ceil(num_images / images_per_row)
     height = tallest * num_rows
@@ -77,7 +86,7 @@ def repo_gif(repo, outfile):
     with open(outfile, 'wb') as fp:
         gifmaker.makedelta(
             fp,
-            frames(reversed(commits), width, height, widest, tallest, file_images.values())
+            frames(commits, width, height, widest, tallest, images)
         )
 
 def frames(commits, width, height, widest, tallest, images):
